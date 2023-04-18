212a3c68926ab8a16f91c87093fc340d571a2d11
DESKTOP-KI971JU\CSG <Usha_MC>
2023-04-18 10:45:09 +0530

    Usha-Portfolio

---
+++ src/Routes/Routes.js
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home";
+import Main from "../Layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [{ path: "/", element: <Home></Home> }],
+  },
+]);
